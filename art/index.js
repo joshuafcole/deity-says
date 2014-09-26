9b8a673e9879cb9e238fea9e4a7bba846794ade4
Joshua Cole <josh.cole@creativelive.com>
2014-09-26 14:17:43 -0700

    More deities, per-deity styles, and multi drawing support

--- art/index.js
+++ art/index.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 var path = require('path');
+var indeks = require('indeks');
 
 function getArt(name) {
-  return fs.readFileSync(path.join(__dirname, name + '.txt'), {encoding: 'utf8'});
+  return fs.readFileSync(name, {encoding: 'utf8'});
 }
 
-module.exports = {
-  pentagram: getArt('pentagram')
-};
+module.exports = indeks.index(__dirname, {loader: getArt, ext: '.txt'});
--- index.js
+++ index.js
@@ -6,27 +6,54 @@ var art = require('./art');
 
 var speakers = [
   {
-    name: 'satan',
-    art: art.pentagram,
+    name: 'Satan',
+    drawings: [art.pentagram],
     messages: [
       'Don\'t sweat it.',
-      'No problem.',
       'That\'ll be twelve souls...',
       'Anything to one up the big man.',
-      'Don\'t mention it. Seriously. Don\'t.'
-    ]
+      'Don\'t mention it. Seriously. Don\'t.',
+      'Your service has been richly rewarded.'
+    ],
+    style: {
+      transformFirst: chalk.red,
+      transformLast: chalk.yellow
+    }
+  },
+  {
+    name: 'Flying Spaghetti Monster',
+    drawings: [art.fsm],
+    messages: [
+      'My noodly appendages will forever by within you.',
+      'Volcanoes of beer await your continued loyalty.',
+      'Don\'t forget to fight global warming by becoming a pirate!'
+    ],
+    style: {
+      transformFirst: chalk.yellow,
+    }
+  },
+  {
+    name: 'Dionysus',
+    drawings: [art.dionysus],
+    messages: [
+      'Thank me with liquor!',
+      'Eat, drink, and be merry with me!',
+      'Ah yes, now, on with the party.'
+    ],
+    style: {
+      transformFirst: chalk.blue,
+      transformLast: chalk.magenta
+    }
   }
 ];
 
 var speaker = util.choose(speakers);
+var drawing = util.choose(speaker.drawings);
 var message = util.choose(speaker.messages);
 var bubble = util.bubblify(message);
 
 function showMessage() {
-  console.log(util.inlineMerge(speaker.art, bubble, {
-    transformFirst: chalk.red,
-    transformLast: chalk.yellow
-  }));
+  process.stdout.write(util.inlineMerge(drawing, bubble, speaker.style));
 }
 
 if(process.stdin.isTTY) {
